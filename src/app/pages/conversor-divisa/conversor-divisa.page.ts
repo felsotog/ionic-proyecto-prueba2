d4fa427fb2315e07607219bb3c0f804b562192be
Felipe <fel.sotog@duocuc.cl>
2021-10-15 01:19:42 -0300

    termino de conversor divisas

--- src/app/pages/conversor-divisa/conversor-divisa.page.ts
+++ src/app/pages/conversor-divisa/conversor-divisa.page.ts
@@ -10,9 +10,15 @@ export class ConversorDivisaPage implements OnInit {
 
   pageTitle = 'Conversor De Divisas'; //Creamos la variable para llamarla del html y muestre el titulo
   
-  usd: number = 0;
-  eur: number = 0;
+  //usd: number = 0;
+  //eur: number = 0;
+  fromValue: any;
+  toValue: any;
+  fromValue2: any;
+  toValue2: any;
   Divisa: any;
+  dolar: any;
+  euro: any;
 
   constructor(private servicedivisaservice: ServiceDivisaService) { }
 
@@ -25,10 +31,30 @@ export class ConversorDivisaPage implements OnInit {
     .subscribe(
       respuesta => {
         this.Divisa = respuesta
+        this.dolar = this.Divisa.dolar.valor
+        this.euro = this.Divisa.euro.valor
         console.log(this.Divisa);
+        console.log(this.dolar);
+        console.log(this.euro);
       },
       err => console.log(err)
     )
+  }
+
+  calculateCurrencyOne() {
+    this.toValue = this.fromValue * this.dolar;
+  }
+
+  calculateCurrencyTwo() {
+    this.fromValue = this.toValue / this.dolar;
+  }
+
+  calculateCurrencyThree() {
+    this.toValue2 = this.fromValue2 * this.euro;
+  }
+
+  calculateCurrencyFour() {
+    this.fromValue2 = this.toValue2 / this.dolar;
   }
 
   /*
